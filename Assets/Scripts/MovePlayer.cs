0f9ce92be76d0adfc742b213a0757f5ee7d5b7d7
Jhonatan David Montañez Espitia <jhonatanmontanez05@gmail.com>
2024-10-18 15:02:57 -0500

    Modificacion de salto y movimiento del personaje
    Modificacion de salto y movimiento del personaje

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -6,6 +6,9 @@ public class MovePlayer : MonoBehaviour
 {
     Rigidbody2D rgbdPlayer;
     public float jumpForce = 1000;
+    private bool tocaSuelo;
+    private float horizontal;
+    public float speed;
     void Start()
     {
         rgbdPlayer = GetComponent<Rigidbody2D>();
@@ -14,9 +17,34 @@ public class MovePlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if( Input.GetKeyDown(KeyCode.Space)){
-            Debug.Log("hola");
-            rgbdPlayer.AddForce(Vector2.up * Time.deltaTime * jumpForce, ForceMode2D.Impulse);
+        horizontal = Input.GetAxis("Horizontal");
+
+        if (horizontal != 0)
+        {
+            transform.Translate(new Vector3(horizontal * speed * Time.deltaTime, 0, 0));
+        }
+
+        if( Input.GetKeyDown(KeyCode.Space)&& tocaSuelo)
+        {
+            rgbdPlayer.velocity = Vector3.zero;
+
+            rgbdPlayer.AddForce(transform.up * jumpForce * Time.deltaTime , ForceMode2D.Impulse );
+            tocaSuelo = false;
+        }
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Suelo"))
+        {
+            tocaSuelo = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag ("Suelo"))
+        {
+            tocaSuelo = false;
         }
     }
 }
