34e759b1d69f0414f61a9b9ea835a55c56a102fa
Angeth86 <90480949+Angeth86@users.noreply.github.com>
2024-10-18 20:00:41 -0500

    canvas

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+
+    [SerializeField] GameObject MenuPanel;
+    [SerializeField] GameObject optionsPanel;
+    [SerializeField] GameObject SettingsPanel;
+
+   
+
+   
+   void Start()
+   {
+       SettingsPanel.SetActive(false);
+   }
+   public void OptionsPanel()
+    {
+
+        //MainPanel.SetActive(false);
+        MenuPanel.SetActive(true);
+       
+
+    }
+    public void play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+     public void Settings()
+    {
+        //  opciones de sonido y video
+        optionsPanel.SetActive(false);
+        SettingsPanel.SetActive(true);
+        //PanelTime.SetActive(false);
+        Debug.Log("opciones");
+    }
+    public void quit()
+    {
+        Debug.Log("Salir");
+        Application.Quit();
+    }
+
+    public void Return(){
+        optionsPanel.SetActive(true);
+        SettingsPanel.SetActive(false);
+       // PanelTime.SetActive(true);
+        Debug.Log("return");
+    }
+}
